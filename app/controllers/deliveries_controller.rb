8a209e83e630ee6bd88e3be23b464e8cab5d5bb2
emontecalvo <emilymontecalvo@gmail.com>
2017-03-11 11:01:05 -0800

    holiday checker in controller

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -10,9 +10,12 @@ class DeliveriesController < ApplicationController
 
 	def create
 		@delivery = Delivery.new(delivery_params)
-		x = is_weekday?
-		p "**************"
-		p x
+		# x = is_weekday?
+		# p "**************"
+		# p x
+		# p "* * * * *  start  date * * * * "
+		# p @delivery.start_date
+		is_holiday?
 
 		if @delivery.save
 			redirect_to @delivery
@@ -31,7 +34,7 @@ class DeliveriesController < ApplicationController
 
 	def update
 		@delivery = Delivery.find(params[:id])
-		if @delivery.update(params[:delivery].permit(:title, :body))
+		if @delivery.update(params[:delivery].permit(:title, :start_date, :num_of_months_between_recur, :delivery_date, :buffer_days))
 			redirect_to @delivery
 		else
 			render 'edit'
@@ -46,7 +49,35 @@ class DeliveriesController < ApplicationController
 	end
 
 	def is_holiday?
-		
+		holidays = [
+			"2017-01-02 00:00:00 UTC",
+			"2017-01-16 00:00:00 UTC",
+			"2017-02-20 00:00:00 UTC",
+			"2017-05-29 00:00:00 UTC",
+			"2017-07-04 00:00:00 UTC",
+			"2017-09-04 00:00:00 UTC",
+			"2017-10-09 00:00:00 UTC",
+			"2017-11-11 00:00:00 UTC",
+			"2017-11-23 00:00:00 UTC",
+			"2017-12-25 00:00:00 UTC",
+			"2018-01-01 00:00:00 UTC",
+			"2018-01-15 00:00:00 UTC",
+			"2018-02-19 00:00:00 UTC",
+			"2018-05-28 00:00:00 UTC",
+			"2018-07-04 00:00:00 UTC",
+			"2018-09-03 00:00:00 UTC",
+			"2018-10-08 00:00:00 UTC",
+			"2018-11-12 00:00:00 UTC",
+			"2018-11-22 00:00:00 UTC",
+			"2018-12-25 00:00:00 UTC",
+		]
+		if holidays.include?(@delivery.start_date.to_s)
+			p " $ $ $ $ $ $ $ $"
+			p "its a HOLIDAY"
+		else
+			p " @ @ @ @ @ @ @ @ @"
+			p "ITS NOT A HOLIDAY"
+		end
 
 	end
 
