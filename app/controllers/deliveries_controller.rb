e5f7a65fa8c78676a0156017417545c9d232e568
emontecalvo <emilymontecalvo@gmail.com>
2017-03-11 09:48:23 -0800

    skeleton setup with blank view files

---
+++ app/controllers/deliveries_controller.rb
@@ -0,0 +1,55 @@
+class DeliveriesController < ApplicationController
+
+	def index
+		@deliveries = Delivery.all
+	end
+
+	def new
+		@deliveries = Delivery.new
+	end
+
+	def create
+		@delivery = Delivery.new(delivery_params)
+
+		if @delivery.save
+			redirect_to @delivery
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@delivery = Delivery.find(params[:id])
+	end
+
+	def edit
+		@delivery = Delivery.find(params[:id])
+	end
+
+	def update
+		@delivery = Delivery.find(params[:id])
+		if @delivery.update(params[:post].permit(:title, :body))
+			redirect_to @delivery
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@delivery = Delivery.find(params[:id])
+		@delivery.destroy
+		
+		redirect_to root_path
+	end
+
+	private
+
+	def delivery_params
+		params.require(:delivery).permit(:title, :start_date, :num_of_months_between_recur, :delivery_date, :buffer_days)
+	end
+
+	def find_delivery
+
+	end
+
+end
